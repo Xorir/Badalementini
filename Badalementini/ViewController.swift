e050fcf0bd26c74d852a174a0c4e66bd18d24d00
Lord Summerisle <lordsummerisle@Lords-MacBook-Pro.local>
2017-05-15 03:08:52 -0400

    annotations and sign in created

--- Badalementini/ViewController.swift
+++ Badalementini/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
 
     @IBOutlet weak var mapView: MKMapView!
     
+    
     var locationManager: CLLocationManager!
     
     override func viewDidLoad() {
@@ -25,15 +26,31 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
             locationManager.requestWhenInUseAuthorization()
             locationManager.startUpdatingLocation()
         }
+        
+        annotationTry()
+    }
+    
+    func annotationTry() {
+        let london = Annotation(title: "London", coordinate: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275), info: "Home to the 2012 Summer Olympics.")
+        let oslo = Annotation(title: "Oslo", coordinate: CLLocationCoordinate2D(latitude: 59.95, longitude: 10.75), info: "Founded over a thousand years ago.")
+        let paris = Annotation(title: "Paris", coordinate: CLLocationCoordinate2D(latitude: 48.8567, longitude: 2.3508), info: "Often called the City of Light.")
+        let rome = Annotation(title: "Rome", coordinate: CLLocationCoordinate2D(latitude: 41.9, longitude: 12.5), info: "Has a whole country inside it.")
+        let washington = Annotation(title: "Washington DC", coordinate: CLLocationCoordinate2D(latitude: 38.895111, longitude: -77.036667), info: "Named after George himself.")
+        let cities = [london, oslo, paris, rome, washington]
+        
+        for city in cities {
+            mapView.addAnnotation(city)
+        }
+
     }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-      if let location = manager.location {
-            let locationValues: CLLocationCoordinate2D = location.coordinate
-        let center = CLLocationCoordinate2D(latitude: locationValues.latitude, longitude: locationValues.longitude)
-        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
-        mapView.setRegion(region, animated: false)
-        }
+//      if let location = manager.location {
+//            let locationValues: CLLocationCoordinate2D = location.coordinate
+//        let center = CLLocationCoordinate2D(latitude: locationValues.latitude, longitude: locationValues.longitude)
+//        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+//        mapView.setRegion(region, animated: false)
+//        }
         
     }
 }
